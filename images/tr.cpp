beb06913bbd8fb10f559c302bdc861d9dd18042b
BhavikJain25 <133594648+BhavikJain25@users.noreply.github.com>
2024-02-08 15:45:08 +0530

    Update tr.cpp

--- images/tr.cpp
+++ images/tr.cpp
@@ -1,1 +1,19 @@
 
+class GamePiece
+{
+    private:
+    virtual bool areSquaresLegal(int srcRow, int srcCol, int destRow, int destCol, GamePiece* gameBoard[8][8]) = 0;
+    char myPieceColor;
+     
+public:
+    GamePiece(char pieceColor) : myPieceColor(pieceColor) {}
+    ~GamePiece() {}
+    virtual char getPiece() = 0;
+    char getColor() {
+        return myPieceColor;
+    }
+   
+    bool isLegalMove(int srcRow, int srcCol, int destRow, int destCol, GamePiece* gameBoard[8][8]) {
+        GamePiece* Dest = gameBoard[destRow][destCol];
+        if ((Dest == 0) || (myPieceColor != Dest->getColor())) {
+            return areSquare
